3aeda9a1dd932b3d707e0881cda003672bba6f20
NomieIsmail <nomieismail71@gmail.com>
2026-07-07 11:08:46 +0800

    Nomie - updated streamlit

--- Practice/Nomie/AIML/app.py
+++ Practice/Nomie/AIML/app.py
@@ -12,3 +12,34 @@ age = st.slider("How old are you?", 1, 100, 25)
 
 if name:
     st.write(f"Nice to meet you, {name}!")
+
+
+import pandas as pd
+data = {
+    "Days": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+    "Cups of Coffee": [1, 2, 4, 3, 5]
+}
+coffee_table = pd.DataFrame(data)
+st.write("Here is the data:")
+st.dataframe(coffee_table)
+
+
+st.write("Here is the visual chart:")
+st.bar_chart(coffee_table, x="Days", y="Cups of Coffee", color = "#005783")
+
+st.divider() # adds a horizontal line
+
+
+
+
+# choice inputs
+ 
+size = st.radio("Pick a size:", ["Small", "Medium", "Large"])
+
+st.divider() # adds a horizontal line
+ 
+country = st.selectbox("Where are you from?", ["India", "Malaysia", "USA", "UK"])
+
+st.divider() # adds a horizontal line
+ 
+skills = st.multiselect("Select your skills:", ["Python", "SQL", "Power BI", "AI"])
